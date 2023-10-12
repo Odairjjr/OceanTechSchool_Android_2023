b2e9438f5e3c953f5f5488aefd0fc649be8aa876
Odairjjr <odairjosebsjunior@gmail.com>
2023-10-11 21:47:05 -0300

    5 New git

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+
     // Retrofit
-    implementation("com.square.retrofit2:retrofit:2.9.0")
-    implementation("com.square.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson")
 }
